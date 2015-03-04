ffc8156740b2b6c457b9730de3f01b1b9512a823
aguformoso <aguformoso@gmail.com>
2015-03-04 17:33:24 -0200

    Script for deploy summary version 1.0

--- ls-deploys.py
+++ ls-deploys.py
@@ -2,22 +2,45 @@
 
 __author__ = 'agustin'
 
-import os, sys
+import os, sys, getopt, locale
+from datetime import datetime
+
+locale.setlocale(locale.LC_TIME, "es_ES")
 
 BASEDIR = '.'
 if len(sys.argv) == 2:
     BASEDIR = sys.argv[1]
 
+apps = dict()
+
 for app in os.listdir(BASEDIR):
 
-    print app
-    if not os.path.isdir(app):
-        print "no es"
+    appDir = "%s/%s" % (BASEDIR, app)
+
+    if not os.path.isdir(appDir):
         continue
 
-    for deploy in os.listdir("%s/%s" % (BASEDIR, app)):
-        if not os.path.isdir(deploy):
+    deploys = []
+    for deploy in os.listdir(appDir):
+
+        deployDir = "%s/%s" % (appDir, deploy)
+
+        if not os.path.isdir(deployDir):
             continue
 
-        print app
-        print deploy
+        if deploy[0] is '.':
+            continue
+
+        date = datetime.strptime(deploy[:8], "%Y%m%d")
+        deploys.append((deploy, date))
+
+    apps[app] = deploys
+
+for app in apps.keys():
+    deploys = apps[app]
+    if len(deploys) == 0:
+        continue
+
+    print app
+    for deploy in deploys:
+        print "\t%s\t%s" % (deploy[0], deploy[1].strftime('%x'))
